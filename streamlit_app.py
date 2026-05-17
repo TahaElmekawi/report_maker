9f0b47bcc14fb662d0cb7c6fd4131212ab06d9b7
Taha Elmekawy <167732304+TahaElmekawi@users.noreply.github.com>
2026-05-18 02:11:49 +0300

    Add CELL_LAT and CELL_LONG to site_sheet

--- streamlit_app.py
+++ streamlit_app.py
@@ -127,7 +127,7 @@ if uploaded_file is not None:
 
         grouped_site['Map'] = grouped_site.apply(lambda row: make_map_link(row['Lat'], row['Lon']), axis=1)
 
-        site_sheet = grouped_site[['Count','CELL_ADDRESS',  'Map', 'First_Use_Date', 'Last_Use_Date']]
+        site_sheet = grouped_site[['Count','CELL_ADDRESS',  'Map', 'CELL_LAT', 'CELL_LONG', 'First_Use_Date', 'Last_Use_Date']]
         site_sheet.columns = ['Count','CELL_ADDRESS',  'Map', 'CELL_LAT', 'CELL_LONG', 'First_Use_Date', 'Last_Use_Date']
 
         # ==============================================
